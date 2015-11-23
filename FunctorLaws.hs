55d5ca37a0e687228255919f65816c5a30151ec0
Touchebag <someone90_6@hotmail.com>
2015-11-23 13:03:03 +0100

    Added type and second law

--- FunctorLaws.hs
+++ FunctorLaws.hs
@@ -1,15 +1,19 @@
 module Maybe where
 
 import Tip
-import qualified Prelude
+import qualified Prelude as P
 
 data Maybe a = Just a | Nothing
 
 fmap :: (a -> b) -> Maybe a -> Maybe b
-fmap _ Nothing = Nothing
+fmap _ Nothing  = Nothing
 fmap f (Just x) = Just (f x)
 
 id :: a -> a
 id a = a
 
-func a b = fmap id === id
+functorLaw1 :: (Maybe a) -> Equality (Maybe a)
+functorLaw1 a = fmap P.id a === P.id a
+
+functorLaw2 :: (Maybe a) -> (b -> c) -> (a -> b) -> Equality (Maybe c)
+functorLaw2 a f g = (fmap f P.. fmap g) a === fmap (f P.. g) a

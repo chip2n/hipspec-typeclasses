9011b798cd9586e77342c3b052823e673088655c
Andreas Arvidsson <andreas.arvidson@gmail.com>
2016-01-11 16:27:37 +0100

    Simplify MultipleInstances example.

--- src/Example/MultipleInstances.hs
+++ src/Example/MultipleInstances.hs
@@ -1,4 +1,4 @@
-module Example.SimpleEq2 where
+module Example.MultipleInstances where
 
 import Tip
 
@@ -6,16 +6,13 @@ data Test = Foo
 data Test2 = Foo2
 
 class Magic z where
-  sorcery1 :: z -> z -> Bool
-  sorcery2 :: z -> z -> Bool
+  sorcery :: z -> z -> Bool
 
 instance Magic Test where
-  sorcery1 Foo Foo = True
-  sorcery2 Foo Foo = False
+  sorcery Foo Foo = True
 
 instance Magic Test2 where
-  sorcery1 Foo2 Foo2 = True
-  sorcery2 Foo2 Foo2 = False
+  sorcery Foo2 Foo2 = True
 
 law :: Magic q => q -> Equality Bool
-law b = bool (sorcery1 b b)
+law b = bool (sorcery b b)

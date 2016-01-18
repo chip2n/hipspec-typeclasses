55d319c72b6b29510ccafee1e8232085e0ed1d05
Touchebag <someone90_6@hotmail.com>
2016-01-18 12:04:55 +0100

    Added test suite

---
+++ src/TestSuite/run_testsuite.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+function emnaRunFile {
+  output=$(emna "$@" 2> /dev/null)
+  local status=$?
+
+  if [ $status -ne 0 ]; then
+    echo -e -n "\033[0;31mERROR: $@"
+  else
+    case "$output" in
+      *Finished* ) echo -e -n "\033[0;32mSUCCESS: $@";;
+      * ) echo -e -n "\033[0;33mWARNING: $@";;
+    esac
+  fi
+
+  echo -e "\033[0m"
+  return $status
+}
+
+for file in ./*.hs
+do
+  emnaRunFile $file
+done
+

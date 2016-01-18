a41020055543fd400439536fb0f7b5eb1db70df6
Touchebag <someone90_6@hotmail.com>
2016-01-18 12:28:36 +0100

    Code cleanup

--- src/TestSuite/run_testsuite.sh
+++ src/TestSuite/run_testsuite.sh
@@ -1,18 +1,27 @@
 #!/bin/bash
 
+# Colours
+RED="\033[0;31m"
+GREEN="\033[0;32m"
+YELLOW="\033[0;33m"
+
 function emnaRunFile {
   output=$(emna "$@" 2> /dev/null)
   local status=$?
 
+  # If not exited correctly
   if [ $status -ne 0 ]; then
-    echo -e -n "\033[0;31mFAILURE: $@"
+    echo -e -n "${RED}FAILURE: $@"
   else
+    # Correct proof prints "Finished!"
+    # If not this is s case we may need to handle differently
     case "$output" in
-      *Finished* ) echo -e -n "\033[0;32mSUCCESS: $@";;
-      * ) echo -e -n "\033[0;33mWARNING: $@";;
+      *Finished* ) echo -e -n "${GREEN}SUCCESS: $@";;
+      * ) echo -e -n "${YELLOW}WARNING: $@";;
     esac
   fi
 
+  # Return colour to normal
   echo -e "\033[0m"
   return $status
 }

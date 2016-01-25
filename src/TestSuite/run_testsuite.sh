d71c1d091a6908de61c098333233bb044fea2a10
Touchebag <someone90_6@hotmail.com>
2016-01-25 13:51:24 +0100

    Went overboard with testsuite script

--- src/TestSuite/run_testsuite.sh
+++ src/TestSuite/run_testsuite.sh
@@ -5,29 +5,79 @@ RED="\033[0;31m"
 GREEN="\033[0;32m"
 YELLOW="\033[0;33m"
 
+function printFail {
+  echo -e -n "${RED}FAILURE: $1"
+}
+
+function printSuccess {
+  echo -e -n "${GREEN}SUCCESS: $1"
+}
+
+function printWarning {
+  echo -e -n "${YELLOW}WARNING: $1"
+}
+
 function emnaRunFile {
-  output=$(emna "$@" 2> /dev/null)
+  output=$(emna "$1" 2> /dev/null)
   local status=$?
+  local returnVal=0
 
   # If not exited correctly
   if [ $status -ne 0 ]; then
-    echo -e -n "${RED}FAILURE: $@"
+    printFail $1
+    returnVal=1
   else
     # Correct proof prints "Finished!"
     # If not this is s case we may need to handle differently
     case "$output" in
-      *Finished* ) echo -e -n "${GREEN}SUCCESS: $@";;
-      * ) echo -e -n "${YELLOW}WARNING: $@";;
+      *Finished* )
+        printSuccess $1
+        returnVal=0
+        ;;
+      * )
+        printWarning $1
+        returnVal=2
+        ;;
     esac
   fi
 
   # Return colour to normal
   echo -e "\033[0m"
-  return $status
+  return $returnVal
 }
 
-for file in ./*.hs
-do
-  emnaRunFile $file
-done
+function testFolder {
+  printf "\nNow running $1 tests\n----------------------\n"
+  fileCntr=0
+  passCntr=0
+  for file in $@/*.hs
+  do
+    fileCntr=$((fileCntr+1))
+    emnaRunFile $file
+    if [ $? -eq 0 ]; then
+      passCntr=$((passCntr+1))
+    fi
+  done
+  printf "Passed $passCntr out of $fileCntr tests\n"
+}
 
+if [ $# -ne 0 ]; then
+  for arg
+  do
+    case "$arg" in
+      Core )
+        testFolder Core
+        ;;
+      Pass )
+        testFolder Pass
+        ;;
+      Fail )
+        testFolder Fail
+        ;;
+    esac
+  done
+else
+  testFolder Core
+  testFolder Pass
+  testFolder Fail
+fi

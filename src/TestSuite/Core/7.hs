2816d0ceb8364fc74351f648ff190a261cdbe0b5
Touchebag <someone90_6@hotmail.com>
2016-02-02 16:17:42 +0100

    Added more test cases

---
+++ src/TestSuite/Core/7.hs
@@ -0,0 +1,13 @@
+module Example.MultipleInstances where
+
+import Tip
+
+data Baz a = Foo a | Bar
+
+instance Eq (Baz a) where
+  (==) (Foo a) (Foo b) = True
+  (==)  Bar     Bar    = True
+  (==)  _       _      = False
+
+law :: Eq a => a -> a -> Equality Bool
+law a b = bool (a == b)

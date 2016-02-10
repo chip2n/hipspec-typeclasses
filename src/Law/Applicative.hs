cd349b1a1cf1c270b20f45c47a6d1b8d7454b9c2
Andreas Arvidsson <andreas.arvidson@gmail.com>
2016-02-10 14:53:28 +0100

    Fix Applicative and Monad laws.

--- src/Law/Applicative.hs
+++ src/Law/Applicative.hs
@@ -1,30 +1,16 @@
 module Law.Applicative where
 
 import Tip
-import qualified Prelude as P
 
-data Maybe a = Just a | Nothing
+applicativeIdentity :: Applicative m => m a -> Equality (m a)
+applicativeIdentity v = (pure id <*> v) === v
 
-pure :: a -> Maybe a
-pure = Just
+applicativeComposition :: Applicative m => m (b -> a) -> m (c -> b) -> m c
+                       -> Equality (m a)
+applicativeComposition u v w = (pure (.) <*> u <*> v <*> w) === (u <*> (v <*> w))
 
-fmap :: (a -> b) -> Maybe a -> Maybe b
-fmap _ Nothing = Nothing
-fmap f (Just x) = Just (f x)
+applicativeHomomorphism :: Applicative m => (a -> b) -> a -> Equality (m b)
+applicativeHomomorphism f x = (pure f <*> pure x) === pure (f x)
 
-(<*>) :: Maybe (a -> b) -> Maybe a -> Maybe b
-Just f <*> m = fmap f m
-Nothing <*> m = Nothing
-
-applicativeIdentity :: Maybe a -> Equality (Maybe a)
-applicativeIdentity v = pure P.id <*> v === v
-
-applicativeComposition :: (Maybe (b -> a)) -> (Maybe (c -> b)) -> (Maybe c)
-                            -> Equality (Maybe a)
-applicativeComposition u v w = pure (P..) <*> u <*> v <*> w === u <*> (v <*> w)
-
-applicativeHomomorphism :: (a -> b) -> a -> Equality (Maybe b)
-applicativeHomomorphism f x = pure f <*> pure x === pure (f x)
-
-applicativeInterchange :: Maybe (a -> b) -> a -> Equality (Maybe b)
-applicativeInterchange u y = u <*> pure y === pure (P.$ y) <*> u
+applicativeInterchange :: Applicative m => m (a -> b) -> a -> Equality (m b)
+applicativeInterchange u y = (u <*> pure y) === (pure ($ y) <*> u)
--- src/Law/Monad.hs
+++ src/Law/Monad.hs
@@ -1,23 +1,12 @@
 module Law.Monad where
 
 import Tip
-import qualified Prelude
--- import Prelude (Maybe(..), (>>=), return)
 
-data Maybe a = Just a | Nothing
+lawLeftIdentity :: Monad m => a -> (a -> m b) -> Equality (m b)
+lawLeftIdentity a f = (return a >>= f) === f a
 
-(>>=) :: Maybe a -> (a -> Maybe b) -> Maybe b
-(Just x) >>= k   = k x
-Nothing  >>= _   = Nothing
+lawRightIdentity :: Monad m => m a -> Equality (m a)
+lawRightIdentity m = (m >>= return) === m
 
-return :: a -> Maybe a
-return = Just
-
-lawLeftIdentity :: a -> (a -> Maybe b) -> Equality (Maybe b)
-lawLeftIdentity a f    = (return a >>= f) === f a
-
-lawRightIdentity :: Maybe a -> Equality (Maybe a)
-lawRightIdentity m     = (m >>= return)   === m
-
-lawAssociativity :: (a -> Maybe b) -> (b -> Maybe c) -> Maybe a -> Equality (Maybe c)
+lawAssociativity :: Monad m => (a -> m b) -> (b -> m c) -> m a -> Equality (m c)
 lawAssociativity f g m = ((m >>= f) >>= g) === (m >>= (\x -> f x >>= g))

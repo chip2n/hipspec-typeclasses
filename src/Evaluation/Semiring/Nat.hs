4da48517330a2bfdf10dd87b130ab82e65d8feec
Touchebag <someone90_6@hotmail.com>
2016-02-28 19:29:39 +0100

    Added Semiring eval examples

---
+++ src/Evaluation/Semiring/Nat.hs
@@ -0,0 +1,44 @@
+module Semiring where
+
+import Tip
+
+data Nat = Zero | Succ Nat
+
+class Semiring a where
+  add :: a -> a -> a
+  mul :: a -> a -> a
+  zero :: a
+  one :: a
+
+instance Semiring Nat where
+  zero = Zero
+  one = Succ Zero
+  add Zero a = a
+  add (Succ a) b = Succ (add a b)
+  mul Zero a = Zero
+  mul (Succ a) b = add b (mul a b)
+
+semiringAddAssoc :: Semiring a => a -> a -> a -> Equality a
+semiringAddAssoc a b c = add (add a b) c === add a (add b c)
+
+semiringAddComm :: Semiring a => a -> a -> a -> Equality a
+semiringAddComm a b c = add a b === add b a
+
+semiringAddId :: Semiring a => a -> Equality a
+semiringAddId a = add a zero === a
+
+semiringMulAssoc :: Semiring a => a -> a -> a -> Equality a
+semiringMulAssoc a b c = mul (mul a b) c === mul a (mul b c)
+
+semiringMulId :: Semiring a => a -> Equality a
+semiringMulId a = mul a one === a
+
+semiringMulDistLeft :: Semiring a => a -> a -> a -> Equality a
+semiringMulDistLeft a b c = mul a (add b c) === add (mul a b) (mul a c)
+
+semiringMulDistRight :: Semiring a => a -> a -> a -> Equality a
+semiringMulDistRight a b c = mul (add a b) c === add (mul a c) (mul b c)
+
+semiringMulAnnih :: Semiring a => a -> Equality a
+semiringMulAnnih a = mul a zero === zero
+

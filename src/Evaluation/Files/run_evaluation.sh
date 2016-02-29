aa592d25a5e87e51580d020ae2be348761c7da1f
Touchebag <someone90_6@hotmail.com>
2016-02-29 16:57:39 +0100

    Now with law-dir

--- src/Evaluation/Files/run_evaluation.sh
+++ src/Evaluation/Files/run_evaluation.sh
@@ -6,10 +6,13 @@ do
   for file in $folder/*.hs
   do
     printf "#### $file ####\n"
+
     TIMEFORMAT=%R
-    TIMETAKEN=$(time (emna -p=z -v=2 $file 2>&1 | sed -e :a -e 's/[^#]#//; ta' > ../Output/$file.txt)  2>&1)
+    TIMETAKEN=$(time (emna -p=z -v=2 --law-dir ../../../../tools/tip-haskell-frontend/src/Tip/Law $file 2>&1 | sed -e ':loop' -e 's/[^]//g' -e 't loop'> ../Output/$file.txt)  2>&1)
+
     echo $TIMETAKEN
     TOTAL=$(echo $TOTAL + $TIMETAKEN | bc -l)
+
   done
   echo "Total time: $TOTAL"
 done

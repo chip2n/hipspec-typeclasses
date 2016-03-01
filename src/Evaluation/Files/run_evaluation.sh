863a8ce5f13cc4302316abdae9f60f5ad744c316
Touchebag <someone90_6@hotmail.com>
2016-03-01 16:04:28 +0100

    Evaluation script now performs rudimentary error checks

--- src/Evaluation/Files/run_evaluation.sh
+++ src/Evaluation/Files/run_evaluation.sh
@@ -5,14 +5,25 @@ do
   TOTAL=0.0
   for file in $folder/*.hs
   do
-    printf "#### $file ####\n"
+    printf "## $file ##\n"
 
     TIMEFORMAT=%R
     TIMETAKEN=$(time (emna -p=z -v=2 --law-dir ../../../../tools/tip-haskell-frontend/src/Tip/Law $file 2>&1 | sed -e ':loop' -e 's/[^]//g' -e 't loop'> ../Output/$file.txt)  2>&1)
 
-    echo $TIMETAKEN
+    FINISHED=$(grep "Finished" ../Output/$file.txt)
+
+    if [ "$FINISHED" == "" ]
+    then
+      printf '\033[0;31m'
+    fi
+
+    printf "$TIMETAKEN\n\n"
+    printf '\033[0m'
+
     TOTAL=$(echo $TOTAL + $TIMETAKEN | bc -l)
 
   done
+  echo "=================="
   echo "Total time: $TOTAL"
+  echo "=================="
 done

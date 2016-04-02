65d8ac0fc67c01004c4bb23c3b710e2e8aa65b82
Andreas Arvidsson <andreas.arvidson@gmail.com>
2016-04-02 17:21:52 +0200

    Remove TODO in one evaluation example.

--- src/Evaluation/Example/Group/SemigroupNatMul.hs
+++ src/Evaluation/Example/Group/SemigroupNatMul.hs
@@ -5,6 +5,5 @@ import Evaluation.Data
 import Evaluation.Semigroup
 import Evaluation.Example.Ring.SemiringNat
 
--- TODO: Includes natAdd in generated TIP code
 instance Semigroup Nat where
     op = natMul

6f9cdd2853cfb68d030a655181d0bb4f3746a08d
Andreas Arvidsson <andreas.arvidson@gmail.com>
2016-05-06 17:04:04 +0200

    Rename variable in SemigroupList example.

--- src/Evaluation/Example/Group/SemigroupList.hs
+++ src/Evaluation/Example/Group/SemigroupList.hs
@@ -5,4 +5,4 @@ import Evaluation.Semigroup
 
 instance Semigroup (List a) where
     Empty       `op` a = a
-    (Cons a as) `op` b = Cons a (as `op` b)
+    (Cons a as) `op` bs = Cons a (as `op` bs)

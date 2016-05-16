b762a00b6b7d91f532fdfc54523e3499e2439b11
Andreas Arvidsson <andreas.arvidson@gmail.com>
2016-05-16 17:07:04 +0200

    Change variable names in semiring instance for matrices.

--- src/Evaluation/Example/Ring/SemiringMatrix2.hs
+++ src/Evaluation/Example/Ring/SemiringMatrix2.hs
@@ -8,10 +8,10 @@ import Evaluation.Semiring
 instance Semiring a => Semiring (Matrix2 a) where
     zero = Matrix2 zero zero zero zero
     one  = Matrix2 one zero zero one
-    add (Matrix2 a1 a2 a3 a4) (Matrix2 b1 b2 b3 b4) =
-        Matrix2 (add a1 b1) (add a2 b2) (add a3 b3) (add a4 b4)
-    mul (Matrix2 a1 a2 a3 a4) (Matrix2 b1 b2 b3 b4) =
-        Matrix2 (add (mul a1 b1) (mul a2 b3))
-                (add (mul a1 b2) (mul a2 b4))
-                (add (mul a3 b1) (mul a4 b3))
-                (add (mul a3 b2) (mul a4 b4))
+    add (Matrix2 a11 a12 a21 a22) (Matrix2 b11 b12 b21 b22) =
+        Matrix2 (add a11 b11) (add a12 b12) (add a21 b21) (add a22 b22)
+    mul (Matrix2 a11 a12 a21 a22) (Matrix2 b11 b12 b21 b22) =
+        Matrix2 (add (mul a11 b11) (mul a12 b21))
+                (add (mul a11 b12) (mul a12 b22))
+                (add (mul a21 b11) (mul a22 b21))
+                (add (mul a21 b12) (mul a22 b22))

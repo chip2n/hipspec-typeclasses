a4d11fd9e7e813ab0f928717a0d95702a4dfd931
Andreas Arvidsson <andreas.arvidson@gmail.com>
2016-05-18 12:37:13 +0200

    Fix too long line in SemiringMatrix2 example

--- src/Evaluation/Example/Ring/SemiringMatrix2.hs
+++ src/Evaluation/Example/Ring/SemiringMatrix2.hs
@@ -9,7 +9,8 @@ instance Semiring a => Semiring (Matrix2 a) where
     zero = Matrix2 zero zero zero zero
     one  = Matrix2 one zero zero one
     add (Matrix2 a11 a12 a21 a22) (Matrix2 b11 b12 b21 b22) =
-        Matrix2 (add a11 b11) (add a12 b12) (add a21 b21) (add a22 b22)
+        Matrix2 (add a11 b11) (add a12 b12)
+                (add a21 b21) (add a22 b22)
     mul (Matrix2 a11 a12 a21 a22) (Matrix2 b11 b12 b21 b22) =
         Matrix2 (add (mul a11 b11) (mul a12 b21))
                 (add (mul a11 b12) (mul a12 b22))

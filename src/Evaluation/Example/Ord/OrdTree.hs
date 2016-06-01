6ea62da3f3a11794ceed26fb86520969087acdc3
Andreas Arvidsson <andreas.arvidson@gmail.com>
2016-06-01 21:17:54 +0200

    Fix indentation in OrdTree example.

--- src/Evaluation/Example/Ord/OrdTree.hs
+++ src/Evaluation/Example/Ord/OrdTree.hs
@@ -6,7 +6,7 @@ import Evaluation.Example.Eq.EqTree
 import Evaluation.Example.Ord.OrdNat
 
 instance Ord a => Ord (Tree a) where
-    (Leaf _) <= (Branch _ _) = True
-    (Leaf x) <= (Leaf y) = x <= y
-    (Branch _ _) <= (Leaf _) = False
+    (Leaf _)     <= (Branch _ _)   = True
+    (Leaf x)     <= (Leaf y)       = x <= y
+    (Branch _ _) <= (Leaf _)       = False
     (Branch l r) <= (Branch l' r') = l == l' && r <= r' || l <= l'
